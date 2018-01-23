96455c6e016ae888aa87f7ea6655aa264805ca17
Patryk Padus <patryk@padus.pl>
2018-01-23 05:16:03 +0100

    Hide branding - tawk.io

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,11 +6,32 @@
 </template>
 
 <script>
+  import $ from 'jquery';
   import cFooter from '~/components/c-footer.vue'
 
   export default {
     components: {
       cFooter
+    },
+    mounted: () => {
+      Tawk_API = Tawk_API || {};
+      Tawk_API.onLoad = function(){
+        Tawk_API.maximize();
+        var TAWK_Handler = $('body').children().filter(function( index ) {
+          const regex = /^(\w+)-(\d+)$/g;
+          const str = $(this).attr('id');
+
+          while ((m = regex.exec(str)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+              regex.lastIndex++;
+            }
+
+            return $(m);
+          }
+        });
+        $(TAWK_Handler).addClass('tawk-customize');
+      };
     }
   }
 </script>
