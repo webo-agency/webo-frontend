095e01b500fb920627d4157e85087960e40f8635
Patryk Padus <patryk@padus.pl>
2018-02-03 14:07:15 +0100

    TawkAPI

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,6 +22,16 @@
       cFooter
     },
     mounted: () => {
+      (function () {
+        var s1 = document.createElement("script");
+        var s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/592181bc76be7313d291db5e/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+      })();
+
       var Tawk_API = Tawk_API || {};
       var Tawk_LoadStart = new Date(); /* eslint no-unused-vars: "off", camelcase: "off" */
 
@@ -59,16 +69,6 @@
         });
         $(TawkHandler).addClass('tawk-customize');
       };
-
-      (function () {
-        var s1 = document.createElement("script");
-        var s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/592181bc76be7313d291db5e/default';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-      })();
     }
   }
 </script>
