15a6ec3ac9a0b1d7853da292f01ef4ab1e3e8367
Patryk Padus <patryk@padus.pl>
2018-04-04 13:27:17 +0200

    Aktualizacja frontendu

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,51 +19,11 @@
 
   export default {
     head: {
-      script: [
-        {src: 'https://embed.tawk.to/592181bc76be7313d291db5e/default', charset: "UTF-8", crossorigin: "*"}
-      ]
     },
     components: {
       cFooter
     },
     mounted: () => {
-      var Tawk_API = Tawk_API || {};
-      var Tawk_LoadStart = new Date(); /* eslint no-unused-vars: "off", camelcase: "off" */
-
-      $("#fast-contact").click(function() {
-        if (typeof Tawk_API === 'object') {
-          if (typeof Tawk_API.maximize === 'function') {
-            Tawk_API.maximize();
-          }
-        }
-      });
-
-      Tawk_API.onChatMaximized = function() {
-        $("body").removeClass("tawk-close").addClass("tawk-open");
-      };
-
-      Tawk_API.onChatMinimized = function() {
-        $("body").removeClass("tawk-open").addClass("tawk-close");
-      };
-
-      Tawk_API.onLoad = function() {
-        $("body").addClass("tawk-loaded");
-        let TawkHandler = $('body').children().filter(function() {
-          const regex = /^(\w+)-(\d+)$/g;
-          const str = $(this).attr('id');
-
-          while (regex.exec(str) !== null) {
-            let m = regex.exec(str);
-            // This is necessary to avoid infinite loops with zero-width matches
-            if (m.index === regex.lastIndex) {
-              regex.lastIndex++;
-            }
-
-            return $(m);
-          }
-        });
-        $(TawkHandler).addClass('tawk-customize');
-      };
     }
   }
 </script>
