1d497f21033611b626c32a62b7062f6e52626ed0
Triloworld <patryk@padus.pl>
2018-08-30 09:26:14 +0200

    Grafika. Stopka

--- components/footer.vue
+++ components/footer.vue
@@ -1,35 +1,51 @@
 <template>
     <div class="footer">
-        <div>
-            //Black bar
-            <!--
-            <div class="footer--section footer--copyright">
-                © 2018
-            </div>
-            <div class="footer--section footer--nip">
-                NIP: 524 24 95 143
-            </div>
-            <div  class="footer--section footer--regon">
-                REGON: 015 690 013
-            </div>
-            <div  class="footer--section footer--logo">
-                <nuxt-link to="/" class="footer--img">
-                    <svg width="100%" height="100%" viewBox="0 0 3088 1038" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
-                    <g transform="matrix(4.16667,0,0,4.16667,0,0)">
-                        <g transform="matrix(1,0,0,1,17.5,-13.5)">
-                            <path d="M650.521,231.244L631.803,231.244L631.803,200.047L663,200.047L663,218.765C663,225.652 657.409,231.243 650.521,231.244ZM619.324,218.765L619.324,231.22L560.05,231.244C541.11,231.244 525.733,215.867 525.733,196.927L525.733,128.294C525.733,109.354 541.11,93.977 560.05,93.977L628.683,93.977C647.623,93.977 663,109.354 663,128.294L663,187.544L650.521,187.544L650.521,134.533C650.521,119.037 637.941,106.456 622.444,106.456L566.289,106.456C550.792,106.456 538.212,119.037 538.212,134.533L538.212,190.688C538.212,206.184 550.793,218.765 566.289,218.765L619.324,218.765Z" style="fill:white;"/>
-                            <path d="M409.151,94.001L468.425,93.977C487.365,93.977 502.742,109.354 502.742,128.294L502.742,196.927C502.742,215.867 487.365,231.243 468.425,231.244L399.792,231.244C380.852,231.243 365.475,215.867 365.475,196.927L365.475,44.756L377.954,44.756L377.954,190.688C377.954,206.184 390.535,218.764 406.031,218.765L462.186,218.765C477.683,218.764 490.263,206.184 490.263,190.688L490.263,134.533C490.263,119.037 477.682,106.456 462.186,106.456L409.151,106.456L409.151,94.001Z" style="fill:white;"/>
-                            <path d="M342.484,231.22L239.534,231.244C220.594,231.244 205.217,215.867 205.217,196.927L205.217,128.294C205.217,109.354 220.594,93.977 239.534,93.977L308.167,93.977C327.107,93.977 342.484,109.354 342.484,128.294L342.484,187.544L248.893,187.544L248.893,175.077L330.005,175.077L330.005,134.533C330.005,119.037 317.425,106.456 301.928,106.456L245.773,106.456C230.276,106.456 217.696,119.037 217.696,134.533L217.696,190.688C217.696,206.184 230.277,218.765 245.773,218.765L342.484,218.765L342.484,231.22Z" style="fill:white;"/>
-                            <path d="M147.909,231.244L142.98,231.244L142.98,218.699C157.849,218.055 169.722,205.774 169.747,190.688L169.747,93.977L182.202,93.977L182.226,196.927C182.226,215.867 166.849,231.244 147.909,231.244ZM55.479,93.977L55.479,190.688C55.504,205.774 67.377,218.055 82.246,218.699L77.317,231.244C58.377,231.244 43,215.867 43,196.927L43.024,93.977L55.479,93.977ZM112.614,231.244L77.317,231.244L82.246,218.699L100.134,218.699L100.131,175.077L112.586,175.077L112.614,231.244Z" style="fill:white;"/>
-                        </g>
-                    </g>
+        <div class="container links justify-content-center justify-content-sm-between">
+            <nuxt-link class="logo" to="/">
+                <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" clip-rule="evenodd" width="167" height="50">
+                    <path fill="#fff" d="M162.885 50h-5.02v-8.365h8.365v5.018c0 1.847-1.5 3.347-3.345 3.347zm-8.365-3.347v3.34L138.627 50c-5.078 0-9.2-4.123-9.2-9.2V22.397c0-5.079 4.122-9.2 9.2-9.2h18.403c5.077 0 9.2 4.121 9.2 9.2v15.886h-3.345V24.071a7.531 7.531 0 0 0-7.528-7.528h-15.056a7.53 7.53 0 0 0-7.528 7.528v15.056a7.53 7.53 0 0 0 7.528 7.526h14.219z"/>
+                    <path fill="#fff" d="M98.17 13.203l15.893-.007c5.078 0 9.2 4.123 9.2 9.2v18.402c0 5.08-4.122 9.2-9.2 9.2H95.66c-5.077 0-9.2-4.12-9.2-9.2V0h3.345v39.125a7.531 7.531 0 0 0 7.528 7.528h15.056a7.53 7.53 0 0 0 7.528-7.528V24.07a7.53 7.53 0 0 0-7.528-7.527H98.17v-3.34z"/>
+                    <path fill="#fff" d="M80.296 49.993L52.692 50c-5.077 0-9.199-4.123-9.199-9.2V22.397c0-5.079 4.122-9.2 9.2-9.2h18.402c5.078 0 9.2 4.121 9.2 9.2v15.886H55.203v-3.343H76.95v-10.87a7.531 7.531 0 0 0-7.528-7.528H54.367a7.53 7.53 0 0 0-7.528 7.528v15.056a7.53 7.53 0 0 0 7.528 7.526h25.929v3.34z"/>
+                    <path fill="#fff" d="M28.127 50h-1.322v-3.364c3.987-.173 7.171-3.466 7.177-7.51V13.198h3.34l.005 27.602c0 5.078-4.121 9.201-9.2 9.201zM3.347 13.197v25.93c.005 4.043 3.189 7.336 7.174 7.51L9.201 50C4.122 50 0 45.877 0 40.8l.006-27.603h3.34zM18.663 50H9.201l1.32-3.364h4.798l-.002-11.695h3.34l.007 15.06z"/>
                 </svg>
-                </nuxt-link>
-            </div>
-            -->
+            </nuxt-link>
+            <ul class="social list-unstyled row mx-4 mx-md-0 flex-grow-1 flex-md-grow-0">
+                <li class="col-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" clip-rule="evenodd" width="24" height="16">
+                        <path fill="#fff" d="M23.35-.003H0V3.11h23.35zM23.344 6.44H7.777v3.114h15.567zM23.355 12.884h-7.783v3.113h7.783z"/>
+                    </svg>
+                </li>
+                <li class="col-auto">
+                    <a class="bh" href="https://www.behance.net/zawadzkimaa9db">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" clip-rule="evenodd" width="26" height="16">
+                            <path fill="#fff" fill-rule="nonzero" d="M23.222 2.27h-6.443V.67h6.444v1.6h-.001zM12.49 8.832c.415.644.623 1.426.623 2.344 0 .95-.234 1.801-.71 2.553-.304.498-.68.918-1.133 1.257-.509.391-1.112.66-1.805.803-.696.144-1.448.216-2.26.216H0v-16h7.727c1.948.032 3.329.596 4.144 1.705.49.68.732 1.495.732 2.443 0 .978-.246 1.76-.74 2.355-.275.334-.68.637-1.218.91.814.299 1.432.768 1.845 1.414zm-8.8-2.519h3.386c.695 0 1.258-.132 1.691-.396.433-.264.649-.733.649-1.407 0-.744-.286-1.238-.86-1.476-.492-.165-1.123-.25-1.888-.25H3.69v3.53zm6.052 4.635c0-.832-.34-1.406-1.018-1.714-.379-.176-.914-.265-1.6-.272H3.69v4.264H7.07c.695 0 1.232-.09 1.62-.28.7-.35 1.051-1.013 1.051-1.998zm15.87-2.603c.078.523.114 1.283.1 2.275h-8.345c.046 1.152.443 1.958 1.198 2.419.454.289 1.006.43 1.651.43.681 0 1.236-.172 1.663-.526.233-.188.438-.452.616-.786h3.058c-.08.68-.448 1.37-1.11 2.071-1.025 1.114-2.463 1.673-4.31 1.673-1.526 0-2.87-.471-4.037-1.41-1.164-.943-1.748-2.471-1.748-4.591 0-1.989.525-3.51 1.577-4.57 1.057-1.06 2.42-1.59 4.1-1.59.996 0 1.894.179 2.695.537.799.358 1.459.922 1.979 1.697.47.683.772 1.472.914 2.37zm-3.01.298c-.056-.796-.322-1.399-.8-1.811-.476-.413-1.069-.62-1.777-.62-.77 0-1.365.221-1.788.657-.425.435-.69 1.026-.798 1.774h5.163z"/>
+                        </svg>
+                    </a>
+                </li>
+                <li class="col-auto">
+                    <a class="fb" href="https://www.facebook.com/meet.webo">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" clip-rule="evenodd" width="8" height="16">
+                            <path fill="#fff" d="M7.146 8.01H4.904v7.994H1.58V8.01H0V5.185h1.58V3.357c0-1.306.62-3.353 3.353-3.353l2.462.01v2.742H5.61c-.294 0-.705.146-.705.77v1.662h2.53l-.288 2.821z"/>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+            <ul class="info list-unstyled row mx-4 mx-md-0 flex-grow-1 flex-md-grow-0">
+                <li class="col-auto">
+                    <nuxt-link to="/contact" class="contact" exact>
+                        SAY hello!
+                    </nuxt-link>
+                </li>
+                <li class="col-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" clip-rule="evenodd" width="18" height="18">
+                        <path fill="#fff" d="M10.368 17.282H-.004V-.003h17.285v10.369c0 3.816-3.1 6.916-6.913 6.916z"/>
+                    </svg>
+                </li>
+            </ul>
         </div>
-        <div>
-            //Grey bar
+        <div class="copyright">
+            <div>NIP: 524 24 95 143 <span class="pipe">|</span> REGON: 015 690 013</div>
+            <div>webo.agency © {{ new Date().getFullYear() }}</div>
         </div>
     </div>
 </template>
@@ -40,41 +56,69 @@
 </script>
 
 <style lang="scss" scoped>
-    .footer {
+    .footer{
+        text-align: center;
+        background: #000;
+    }
+
+    // First bar
+    .footer .logo{
+        width: 100%;
+        order: 0;
+        padding-top: 20px;
+        padding-bottom: 20px;
+
+        @include media-breakpoint-up('md'){
+            order: 2;
+            width: auto;
+        }
+    }
+
+    .footer .links{
         display: flex;
-        flex-direction: column-reverse;
+        flex-direction: row;
         flex-wrap: wrap;
         align-items: center;
-        justify-content: space-around;
-        width: 100%;
-        color: #fff;
-        min-height: 200px;
-
-        @media(min-width: 877px){
-            flex-direction: row;
-        }
+        justify-content: space-between;
+        padding-top: 20px;
+        padding-bottom: 20px;
     }
 
-    .footer--section{
+    .footer .social,
+    .footer .info{
         display: flex;
+        flex-direction: row;
         align-items: center;
-        justify-content: center;
-        flex: 1 1 auto;
+        justify-content: space-between;
+        margin-bottom: 0;
     }
 
-    .footer--section.footer--copyright{
-        justify-content: flex-end;
+    .footer .social{
+        order: 1;
     }
 
-    .footer--section.footer--logo{
-        justify-content: center;
-
-        @media(min-width: 877px){
-            justify-content: flex-start;
-        }
+    .footer .info{
+        order: 3;
     }
 
-    .footer--img{
-        max-width: 100px;
+    .footer .social .bh,
+    .footer .social .fb,
+    .footer .info .contact{
+        padding-top: 20px;
+        padding-bottom: 20px;
+        color: #fff;
+    }
+
+
+    // Second bar
+
+    .footer .copyright{
+        background: #282524;
+        padding: 13px;
+        color: #fff;
+    }
+
+    .footer .copyright .pipe{
+        color: #16AB86;
     }
 </style>
