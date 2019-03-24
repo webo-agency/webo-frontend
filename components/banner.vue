5a5f6f1a0445a5b7cb2185f75e76c622820cc551
Triloworld <patryk@padus.pl>
2019-03-24 12:46:07 +0100

    Test innego sposobu poruszania

--- components/banner.vue
+++ components/banner.vue
@@ -76,14 +76,92 @@
         }
       );
 
-      const elBody = document.querySelector('body');
-      const el = document.querySelector("#img");
-      elBody.addEventListener("mousemove", (e) => {
-        if(e.offsetX !== 0 && e.offsetY !== 0){
-          el.style.objectPosition = `-${Math.abs(e.offsetX) / 15}px -${Math.abs(e.offsetY) / 15}px`;
-        }
-      });
+      // const elBody = document.querySelector('body');
+      // const el = document.querySelector("#img");
+      // elBody.addEventListener("mousemove", (e) => {
+      //   if(e.offsetX !== 0 && e.offsetY !== 0){
+      //     el.style.objectPosition = `-${Math.abs(e.offsetX) / 15}px -${Math.abs(e.offsetY) / 15}px`;
+      //   }
+      // });
 
+
+      (function() {
+        // Init
+        var container = document.getElementById(this.mainId),
+          inner = document.getElementById("img");
+
+        // Mouse
+        var mouse = {
+          _x: 0,
+          _y: 0,
+          x: 0,
+          y: 0,
+          updatePosition: function(event) {
+            var e = event || window.event;
+            this.x = e.clientX - this._x;
+            this.y = (e.clientY - this._y) * -1;
+          },
+          setOrigin: function(e) {
+            this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+            this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+          },
+          show: function() {
+            return "(" + this.x + ", " + this.y + ")";
+          }
+        };
+
+        // Track the mouse position relative to the center of the container.
+        mouse.setOrigin(container);
+
+        //-----------------------------------------
+
+        var counter = 0;
+        var updateRate = 10;
+        var isTimeToUpdate = function() {
+          return counter++ % updateRate === 0;
+        };
+
+        //-----------------------------------------
+
+        var onMouseEnterHandler = function(event) {
+          update(event);
+        };
+
+        var onMouseLeaveHandler = function() {
+          inner.style = "";
+        };
+
+        var onMouseMoveHandler = function(event) {
+          if (isTimeToUpdate()) {
+            update(event);
+          }
+        };
+
+        //-----------------------------------------
+
+        var update = function(event) {
+          mouse.updatePosition(event);
+          updateTransformStyle(
+            (mouse.y / inner.offsetHeight / 2).toFixed(2),
+            (mouse.x / inner.offsetWidth / 2).toFixed(2)
+          );
+        };
+
+        var updateTransformStyle = function(x, y) {
+          var style = "translate(" + x + "px," + y + "px)";
+          inner.style.transform = style;
+          inner.style.webkitTransform = style;
+          inner.style.mozTransform = style;
+          inner.style.msTransform = style;
+          inner.style.oTransform = style;
+        };
+
+        //-----------------------------------------
+
+        container.onmouseenter = onMouseEnterHandler;
+        container.onmouseleave = onMouseLeaveHandler;
+        container.onmousemove = onMouseMoveHandler;
+      })();
 
     },
     methods: {
