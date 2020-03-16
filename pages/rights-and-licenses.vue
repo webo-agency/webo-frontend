5cd4e2ef9d0b70a0f83e445039ca00527e2c5fec
Triloworld <patryk@padus.pl>
2020-03-16 21:05:02 +0100

    SASS removed

--- pages/rights-and-licenses.vue
+++ pages/rights-and-licenses.vue
@@ -43,14 +43,14 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .page {
   width: 100%;
   margin: 25vh auto;
 }
 </style>
 
-<style lang="scss">
+<style>
 .page h2 {
   margin-bottom: 15px;
 }
