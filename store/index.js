d6a5abd480dc02a9418add6770c32e3684f4efe8
Triloworld <patryk@padus.pl>
2020-04-29 14:57:49 +0200

    Resolve multiple request in async manner

--- store/index.js
+++ store/index.js
@@ -6,18 +6,20 @@ export const actions = {
   async nuxtServerInit({ dispatch }) { // eslint-disable-line
     await dispatch('dataInit');
   },
-  async dataInit({ dispatch }) {
-  
-    let resOptions = await axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/acf/v3/options/options').concat('?lang=pl'));
-    let resMenu = await axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus').concat('?lang=pl'));
-    let resSubMenu1 = await axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus/').concat('social-media').concat('?lang=pl'));
-    let resSubMenu2 = await axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus/').concat('uslugi').concat('?lang=pl'));
-    let resSubMenu3 = await axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus/').concat('webo').concat('?lang=pl'));
-    
-    await dispatch('general/init', resOptions.data.acf);
-    await dispatch('menu/mainInit', resMenu.data);
-    await dispatch('menu/submenuInit', resSubMenu1.data);
-    await dispatch('menu/submenuInit', resSubMenu2.data);
-    await dispatch('menu/submenuInit', resSubMenu3.data);
+  dataInit({ dispatch }) {
+    return axios.all([
+      axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/acf/v3/options/options').concat('?lang=pl')),
+      axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus').concat('?lang=pl')),
+      axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus/').concat('social-media').concat('?lang=pl')),
+      axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus/').concat('uslugi').concat('?lang=pl')),
+      axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/menus/v1/menus/').concat('webo').concat('?lang=pl'))
+    ])
+    .then(axios.spread(async (resOptions, resMenu, resSubMenu1, resSubMenu2, resSubMenu3) => {
+      await dispatch('general/init', resOptions.data.acf);
+      await dispatch('menu/mainInit', resMenu.data);
+      await dispatch('menu/submenuInit', resSubMenu1.data);
+      await dispatch('menu/submenuInit', resSubMenu2.data);
+      await dispatch('menu/submenuInit', resSubMenu3.data);
+    }));
   }
 }
