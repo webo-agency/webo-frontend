00ba2cac0ce7ee412c2d072f27eeb28688b703ca
Tomek A <tomasz_adamczyk@hotmail.com>
2020-05-25 14:41:42 +0200

    Opinions slider component

---
+++ components/opinions-slider.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div
+      v-swiper:opinionsSM="opinionOptionsSM"
+      class="m-0 lg:hidden"
+    >
+      <div class="swiper-wrapper lg:flex-wrap justify-between h-auto">
+        <div class="empty-opinion-space" />
+        <Opinion
+          v-for="(opinion, index) in slides"
+          :key="index"
+          :title="opinion.title"
+          :content="opinion.content"
+          :author="opinion.author"
+          :company="opinion.company"
+          class="swiper-slide w-auto"
+        />
+      </div>
+      <div class="opinion-pagination" />
+    </div>
+    <div
+      v-swiper:opinionsLG="opinionOptionsLG"
+      class="m-0 hidden lg:block"
+    >
+      <div class="swiper-wrapper justify-between h-auto pb-8">
+        <div
+          v-for="(slide, index) in generateSlideBoards"
+          :key="index"
+          class="flex flex-wrap justify-between swiper-slide w-auto"
+        >
+          <div class="empty-opinion-space" />
+          <Opinion
+            v-for="opinion in slide"
+            :key="opinion.id"
+            :title="opinion.title"
+            :content="opinion.content"
+            :author="opinion.author"
+            :company="opinion.company"
+          />
+        </div>
+        <div class="opinion-pagination" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OpinionsSlider",
+  props: {
+    slides: {
+      type: Array,
+      default: () => [],
+      required: false,
+    },
+  },
+  data() {
+    return {
+      opinionOptionsSM: {
+        autoplay: 2000,
+        slidesPerView: 2,
+        spaceBetween: 50,
+        pagination:  {
+            el: '.opinion-pagination',
+            clickable: true,
+        }, //{el: '.swiper-pagination', clickable: true}
+        breakpoints: {
+          768: {
+              slidesPerView: 1
+          },
+        },
+      },
+      opinionOptionsLG: {
+        autoplay: 2000,
+        slidesPerView: 1,
+        spaceBetween: 50,
+        pagination: {
+            el: '.opinion-pagination',
+            clickable: true,
+        }, //{el: '.swiper-pagination', clickable: true}
+      },
+    };
+  },
+  computed: {
+    generateSlideBoards() {
+        const slideSize = 4;
+        const chunk = (sourceArray, chunkSize) =>
+        Array.from({ length: Math.ceil(sourceArray.length / chunkSize) }, (v, i) =>
+            sourceArray.slice(i * chunkSize, i * chunkSize + chunkSize)
+        );
+        const largeDeviceSlides = chunk(this.slides, slideSize);
+        return largeDeviceSlides;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .empty-opinion-space {
+    display: none;
+    margin-bottom: 70px;
+    
+    @media (min-width: 1024px) {
+      display: block;
+      max-width: 26%;
+      flex: 0 0 26%;
+    }
+  }
+
+  .opinion-slide {
+    display: flex;
+  }
+
+  .opinion-pagination {
+
+    .swiper-pagination-bullet {
+      border-radius: none!important;
+      width: 15px!important;
+      height: 15px!important;
+
+      &-active {
+        background: rgb(0, 224, 180);
+      }
+    }
+  }
+</style>
