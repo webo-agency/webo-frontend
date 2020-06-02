543ec0e525fe9a51f94d0391a675537d7dfef2f0
Tomek A <tomasz_adamczyk@hotmail.com>
2020-06-02 21:01:13 +0200

    implementing articleType props into article list

--- components/article-list.vue
+++ components/article-list.vue
@@ -4,12 +4,17 @@
     :id="mainId"
     :class="mainClass"
   >
-    <div class="w-full flex flex-col xs:flex-row flex-wrap">
+    <div
+      class="w-full flex flex-col xs:flex-row flex-wrap"
+      :class="articleListClass"
+    >
       <article-block
         v-for="(item, index) in articleListInject"
         :key="index"
         :article="item"
-        main-class="flex-initial w-full sm:w-1/2 lg:w-1/3 mt-5 mb-5 md:mt-10 md:mb-10 xs:pr-4 md:pr-0"
+        main-class="flex-initial w-full sm:w-1/2 xs:pr-4 md:pr-0"
+        :article-type="articleType"
+        :class="articleClass"
       />
       <article-block
         v-if="JSON.stringify(more) != '{}'"
@@ -68,9 +73,32 @@
         type: String,
         default: "",
         required: false
-      }
+      },
+      articleType: {
+        type: String,
+        default: "default",
+        required: false,
+      },
     },
     computed: {
+      articleClass: function() {
+        switch(this.articleType) {
+          case "service":
+            return 'lg:w-full-8/2 mt-2 mb-2 service';
+
+          default:
+            return 'lg:w-1/3 mt-5 mb-5 md:mt-10 md:mb-10 text-black rounded-md p-5';
+        }
+      },
+      articleListClass: function() {
+        switch(this.articleType) {
+          case "service":
+            return 'justify-between';
+
+          default:
+            return '';
+        }
+      },
       articleListInject: function(){
         let articles = [];
         
@@ -99,3 +127,6 @@
     }
   };
 </script>
+
+<style lang="scss" scoped>
+</style>
