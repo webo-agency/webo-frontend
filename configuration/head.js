0883a017b3967dd3c0008fcbf23ff6b57c868fa9
Triloworld <patryk@padus.pl>
2020-06-05 18:55:49 +0200

    Sections deploy

--- configuration/head.js
+++ configuration/head.js
@@ -1,65 +1,65 @@
 export default {
-  title: "webo - Digital partners",
+  title: "webo - Cyfrowy partner",
   meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
       hid: "description",
       name: "description",
-      content: "Digital freaks in creating web and design"
-      },
-      { name: "msapplication-TileColor", content: "#ffffff" },
-      { name: "msapplication-TileImage", content: "/mstile-144x144.png" },
-      { name: "theme-color", content: "#ffffff" },
-      { name: "robots", content: "INDEX,FOLLOW" },
+      content: "Rozwiązania cyfrowe takie jak strony web czy projektowanie budowane w myśl procesu agile"
+    },
+    { name: "msapplication-TileColor", content: "#ffffff" },
+    { name: "msapplication-TileImage", content: "/mstile-144x144.png" },
+    { name: "theme-color", content: "#ffffff" },
+    { name: "robots", content: "INDEX,FOLLOW" },
   ],
   script: [
-      // { src: `https://polyfill.io/v3/polyfill.min.js?features=${features}&flags=gated`, body: true, async: true, defer: true  },
+    // { src: `https://polyfill.io/v3/polyfill.min.js?features=${features}&flags=gated`, body: true, async: true, defer: true  },
   ],
   link: [
-      {
+    {
       rel: "icon",
       type: "image/x-icon",
       href: "/favicon.ico"
-      },
-      {
+    },
+    {
       rel: "apple-touch-icon",
       sizes: "180x180",
       href: "/apple-touch-icon.png"
-      },
-      {
+    },
+    {
       rel: "icon",
       type: "image/png",
       sizes: "32x32",
       href: "/favicon-32x32.png"
-      },
-      {
+    },
+    {
       rel: "icon",
       type: "image/png",
       sizes: "194x194",
       href: "/favicon-194x194.png"
-      },
-      {
+    },
+    {
       rel: "icon",
       type: "image/png",
       sizes: "192x192",
       href: "/android-chrome-192x192.png"
-      },
-      {
+    },
+    {
       rel: "icon",
       type: "image/png",
       sizes: "16x16",
       href: "/favicon-16x16.png"
-      },
-      {
+    },
+    {
       rel: "mask-icon",
       href: "/safari-pinned-tab.svg",
       color: "#262626"
-      },
-      {
+    },
+    {
       rel: "preconnect",
       href: "https://fonts.gstatic.com",
       crossorigin: true
-      }
+    }
   ]
 }
--- store/index.js
+++ store/index.js
@@ -1,11 +1,25 @@
-import axios from 'axios';
+import axios from 'axios'
+import filter from 'lodash/filter';
+import indexOf from 'lodash/indexOf';
 
-export const mutations = {}
+export const state = () => ({
+  data: {}
+});
+
+export const mutations = {
+  save(state, data) {
+      state.data = { ...state.data, ...data };
+  }
+};
+
+export const getters = {}
+
 export const actions = {
     // nuxtServerInit is called by Nuxt.js before server-rendering every page
   async nuxtServerInit({ dispatch }) { // eslint-disable-line
     await dispatch('dataInit');
   },
+
   dataInit({ dispatch }) {
     return axios.all([
       axios.get(process.env.API_URL.concat(process.env.API_AFFIX).concat('/wp/v2/posts/').concat('?categories=9')),
@@ -23,5 +37,94 @@ export const actions = {
       await dispatch('menu/submenuInit', resSubMenu2.data);
       await dispatch('menu/submenuInit', resSubMenu3.data);
     }));
+  },
+
+  async initHomepage (context, $wp) {
+
+      let frontpage = await $wp.frontPage(20);
+      let pages = await $wp.pages().perPage(20);
+      // let technology = await $wp.technology().perPage(20);
+      let posts = await $wp.posts();
+      let data = frontpage;
+      
+      // let pages = context.app.$wp.pages().perPage(20);
+      // let technology = context.app.$wp.technology().perPage(20); //// Too much
+
+      data.acf.services_promoted = filter(pages, (page) => data.acf.services_promoted.indexOf(page.id) > -1);
+  
+      data.acf.company_promoted = filter(pages, (page) => data.acf.company_promoted.indexOf(page.id) > -1);
+    
+      //Was too much
+      // data.acf.technology_promoted = [];
+      // data.acf.technology_promoted_list.forEach((promoted) => {
+      //   technology.forEach((entry_list) => {
+      //     if(entry_list.id == promoted.term_id){
+      //       data.acf.technology_promoted.push([entry_list])
+      //     }
+      //   });
+      // });
+
+      // data.acf.category_technology_promoted = [];
+      // data.acf.category_technology_promoted_list.forEach((promoted) => {
+      //   technology.forEach((entry_list) => {
+      //     if(entry_list.id == promoted.term_id){
+      //       data.acf.category_technology_promoted.push(entry_list);
+      //     }
+      //   });
+      // });
+
+      // let _childs_category_technology_promoted = [];
+      // for (let cat_promoted_technology of data.acf.category_technology_promoted) {
+      //   cat_promoted_technology.childs = [];
+        
+      //   technology.forEach((technology) => {
+      //     if(cat_promoted_technology.id == technology.parent){
+      //       cat_promoted_technology.childs.push(technology);
+      //     } 
+      //   });
+    //!Was too much
+
+      //   _childs_category_technology_promoted.push(cat_promoted_technology);
+      // }
+      // data.acf.category_technology_promoted = _childs_category_technology_promoted;
+
+      // data.acf.reviews_promoted = [
+      //     await app.$wp.pages().id(data.acf.reviews_promoted),
+      // ];
+
+      // let projects_carousel_list = context.app.$wp.posts().include(data.acf.projects_carousel);
+      
+      let projects_carousel_list = filter(posts, (v) => indexOf(data.acf.projects_carousel, v.id) !== -1);
+      
+      data.acf.projects_carousel_list = [];
+      for(let i = 0; i < data.acf.projects_carousel.length; i++){
+        let entry = projects_carousel_list[i];
+        
+        data.acf.projects_carousel_list.push({
+          id: entry.id,
+          url: entry.acf.project_promoted_screensave.url,
+          alt: entry.acf.project_promoted_screensave.alt,
+          title: entry.acf.project_promoted_title
+        });
+      }
+      data.acf.projects_carousel_list.reverse();
+
+      // let brands_slajder_list = posts;
+      // let brands_slajder_list = context.app.$wp.posts().include(data.acf.brands_slajder);
+      let brands_slajder_list = filter(posts, (v) => indexOf(data.acf.brands_slajder, v.id) !== -1);
+      data.acf.brands_slajder_list = [];
+      for(let i = 0; i < data.acf.brands_slajder.length; i++){
+        let entry = brands_slajder_list[i];
+        
+        data.acf.brands_slajder_list.push({
+          id: entry.id,
+          url: entry.acf.mark_logo.url,
+          alt: entry.acf.mark_logo.alt,
+          href: entry.acf.mark_url,
+        });
+      }
+      data.acf.brands_slajder_list.reverse();
+
+    context.commit('save', data);
   }
 }
