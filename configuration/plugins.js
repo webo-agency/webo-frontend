cb9dcf9c753d008abbc9b7d2f2c4dd5aab8da9e9
Triloworld <patryk@padus.pl>
2020-06-08 20:52:23 +0200

    Web Fon Loader options

--- configuration/plugins.js
+++ configuration/plugins.js
@@ -1,5 +1,5 @@
 export default [
-  { src: "~/plugins/scrollactive.js" },
+  { src: "~/plugins/scrollactive.js", mode: 'client' },
   { src: "~/plugins/swiper.js", mode: 'client' },
   // { src: "~/plugins/wpapi-extend.js", ssr: true}
   { src: '~/plugins/webfontloader.js', mode: 'client' }
