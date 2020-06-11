76f413193df3b25eb1d42f7798caa8e975dcdba3
Triloworld <patryk@padus.pl>
2020-06-11 21:29:07 +0200

    Title changes

--- configuration/head.js
+++ configuration/head.js
@@ -1,5 +1,5 @@
 export default {
-  title: "webo - Cyfrowy partner",
+  title: "webo - Partner w cyfrowej transformacji",
   meta: [
     { charset: "utf-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
