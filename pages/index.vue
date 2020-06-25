1d788b0bb90c7fdfc4fb6833e8435ebbe65cd608
Triloworld <patryk@padus.pl>
2020-06-25 22:31:09 +0200

    Technology disable

--- pages/index.vue
+++ pages/index.vue
@@ -187,11 +187,11 @@
           class="mb-10 w-full md:w-4/5"
           v-html="frontPageData.technology_settings.description"
         />
-        <TechnologyBlock
+        <!--<TechnologyBlock
           class="md:absolute top-0 md:w-half-screen box-position"
           :technologies="frontPageData.technologies"
           :promoted-technologies="frontPageData.category_technology_promoted_list"
-        />
+        />-->
       </div>
     </SectionWrapper>
     <SectionWrapper
