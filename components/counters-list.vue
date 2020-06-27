ef689c4b23c6db3dab78be287dced6ed91f248e9
Tomek A <tomasz_adamczyk@hotmail.com>
2020-06-27 22:57:17 +0200

    WEBOWEB-9 renderer of counters added

---
+++ components/counters-list.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="flex flex-wrap max-w-85%">
+    <CounterItem
+      v-for="item in counters"
+      :key="item.id"
+      :count-value="item.count"
+      :count-title="item.title"
+    />
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CountersList',
+  props: {
+    counters: {
+      type: Array,
+      default: function(){ return []; },
+      required: false,
+    }
+  }
+}
+</script>
