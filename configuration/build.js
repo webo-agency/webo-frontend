b54982e6bab235dd6e490afc9f5f1580887c9522
Triloworld <patryk@padus.pl>
2020-06-27 14:52:19 +0200

    Minify disable

--- configuration/build.js
+++ configuration/build.js
@@ -51,22 +51,22 @@ export default {
     delete vueRule.loader;
     delete vueRule.options;
   },
-  html :{
-    minify: {
-      // collapseWhitespace: true,
-      conservativeCollapse: false,
-      collapseBooleanAttributes: false,
-      decodeEntities: false,
-      minifyCSS: true,
-      // minifyJS: true,
-      processConditionalComments: true,
-      removeEmptyAttributes: true,
-      removeRedundantAttributes: true,
-      trimCustomFragments: true,
-      useShortDoctype: true,
-      removeComments: false
-    }
-  },
+  // html :{
+  //   minify: {
+  //     // collapseWhitespace: true,
+  //     conservativeCollapse: false,
+  //     collapseBooleanAttributes: false,
+  //     decodeEntities: false,
+  //     minifyCSS: true,
+  //     // minifyJS: true,
+  //     processConditionalComments: true,
+  //     removeEmptyAttributes: true,
+  //     removeRedundantAttributes: true,
+  //     trimCustomFragments: true,
+  //     useShortDoctype: true,
+  //     removeComments: false
+  //   }
+  // },
   postcss: {
     plugins: {
       tailwindcss: join(__dirname, '..', 'tailwind.config.js'),
