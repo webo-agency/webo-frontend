6e25f3545360a61a35559c42ad9a47244e739b10
Tomek <tomasz_adamczyk@hotmail.com>
2021-04-01 11:14:01 +0200

    WEBOWEB-46 use viewport directive in vue project

--- configuration/plugins.js
+++ configuration/plugins.js
@@ -2,5 +2,6 @@ export default [
   { src: "~/plugins/scrollactive.js", mode: 'client' },
   { src: "~/plugins/swiper.js", mode: 'client' },
   { src: '~/plugins/webfontloader.js', mode: 'client' },
-  { src: '~/plugins/normalize.js' }
+  { src: '~/plugins/normalize.js' },
+  { src: '~/plugins/viewport-directive.js' }
 ];
