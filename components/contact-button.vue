8395775e1d438c12842edb9f2f04b5e96fd5799a
Tomek <tomasz_adamczyk@hotmail.com>
2021-04-06 19:54:54 +0200

    WEBOWEB-23 button css adjustments

--- components/contact-button.vue
+++ components/contact-button.vue
@@ -4,7 +4,7 @@
     :id="mainId"
     :class="mainClass"
     :href="link"
-    class="button-primary block py-1 px-8"
+    class="button"
   >
     <span>
       {{ text }}
@@ -49,3 +49,83 @@
     },
   };
 </script>
+
+<style lang="postcss" scoped>
+
+  .button {
+    @apply inline-block;
+    @apply py-3;
+    @apply font-bold;
+    @apply px-8;
+    @apply text-btn;
+    @apply border;
+    -webkit-appearance: none;
+
+    @screen md {
+      @apply text-base;
+    }
+
+    &-small {
+      @apply text-btn;
+      @apply px-3;
+      @apply py-1;
+    }
+    
+    &-primary {
+      @apply text-darkText;
+      @apply bg-main;
+      @apply border-main;
+
+      &:hover {
+        @apply bg-transparent;
+        @apply text-main;
+      }
+    }
+
+    &-with-line {
+      @apply flex;
+      @apply items-center;
+
+      &::after {
+        content: '';
+        width: 20px;
+        height: 2px;
+        @apply block;
+        @apply ml-3;
+      }
+    }
+    
+    &-with-line-primary {
+
+      &::after {
+        @apply bg-main;
+      }
+
+      &:hover::after {
+        @apply bg-backgroundDark;
+      }
+    }
+    
+    &-with-line-dark {
+
+      &::after {
+        @apply bg-backgroundDark;
+      }
+
+      &:hover::after {
+        @apply bg-main;
+      }
+    }
+
+    &-outline-primary {
+      @apply bg-transparent;
+      @apply border-main;
+      @apply text-main;
+
+      &:hover {
+        @apply text-darkText;
+        @apply bg-main;
+      }
+    }
+  }
+</style>
